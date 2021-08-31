e91328bf9e892e2d14eb4da8465e0545879e8b1d
judahlibera <73613721+judahlibera@users.noreply.github.com>
2021-08-31 13:33:31 -0400

    Add files via upload

---
+++ empty.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class empty {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        System.out.printf("Enter a noun ");
+        String noun = sc.nextLine();
+        System.out.printf("Enter a verb ");
+        String verb = sc.nextLine();
+        System.out.printf("Enter an adjective ");
+        String adj = sc.nextLine();
+        System.out.printf("Enter an adverb ");
+        String adv = sc.nextLine();
+        System.out.printf("Do you %s your %s %s %s? That's Hilarious!", verb, adj, noun, adv);
+    }
+}
